bbf6f545f0e9c8b25de15fa30a5c31bee9e63cd4
rochaecp <rochaecp@gmail.com>
2024-07-12 18:05:39 -0300

    insere contato de whatsapp e organiza as imagens do catalogo noutra pasta

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -5,7 +5,7 @@ function createCard(livro) {
     return `
         <div class="col-lg-3 col-md-4 col-sm-6">
             <div class="card">
-                <img src="imagens/${livro.arquivo}" class="card-img-top" data-bs-toggle="modal" data-bs-target="#modal-${removeFileExtension(livro.arquivo)}">
+                <img src="imagens/catalogo/${livro.arquivo}" class="card-img-top" data-bs-toggle="modal" data-bs-target="#modal-${removeFileExtension(livro.arquivo)}">
                 <div class="card-body">
                     <h5 class="card-title">${livro.titulo}</h5>
                     <p class="card-text">${livro.autor}</p>
@@ -19,8 +19,13 @@ function createCard(livro) {
                             <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                         </div>
                         <div class="modal-body">
-                            <img src="imagens/${livro.arquivo}" class="img-fluid">
+                            <img src="imagens/catalogo/${livro.arquivo}" class="img-fluid">
                         </div>
+                        <div class="modal-footer">
+                            <p class="mb-1 me-3">Mais informações ou compras via 
+                                <a href="https://wa.me/555132240309" target="_blank">WhatsApp<img src="imagens/wpp_icon.png" alt="WhatsApp" style="width: 20px; height: 20px; margin-left: 6px"></a>.
+                            </p>
+                        </div>                        
                     </div>
                 </div>
             </div>
